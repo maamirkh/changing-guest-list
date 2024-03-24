c87bab389b9058933c15e6024bed3942223467bd
maamirkh <maamirkhan1983@gmail.com>
2024-03-25 01:07:27 +0500

    change my guest list

---
+++ app.ts
@@ -0,0 +1,16 @@
+#! usr/bin/env node
+let guests: string[] = ["Ahmed Ebrahim", "Danish Anwer", "Fahim Saleem"];
+guests.forEach(guest => {
+    console.log(`Dear ${guest}, would you like to join me for dinner?`);
+});
+let unableToAttend = "Ahmed Ebrahim";
+console.log(`${unableToAttend} can't make it to dinner.`);
+
+// Replace the guest
+let newGuest = "Sufyan Liaquat";
+guests[guests.indexOf(unableToAttend)] = newGuest;
+
+// New invitations
+guests.forEach(guest => {
+    console.log(`Dear ${guest}, would you like to join me for dinner?`);
+});
